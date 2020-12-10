17d15a1683524a7584c08835c5fceb281d9cdcad
cst <2757045143@qq.com>
2020-12-10 23:32:19 +0800

    #comment [add] 通用fastapi 改进

--- code/app/utils.py
+++ code/app/utils.py
@@ -1,5 +1,9 @@
-from functools import wraps
+import logging
+import requests
 from enum import Enum
+from functools import wraps
+from retrying import retry
+from app.exceptions import HTTPError
 
 
 class ResponseCode(Enum):
@@ -9,9 +13,15 @@ class ResponseCode(Enum):
     Unknow = -2
 
 
-class InterCode(Enum):
-    """内部响应码"""
-    pass
+class Logger:
+
+    def __init__(self, level=logging.INFO, name='root'):
+        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        logging.basicConfig(level=level, format=format)
+        self.logger = logging.getLogger(name)
+
+
+logger = Logger().logger
 
 
 def make_response(func):
@@ -20,8 +30,23 @@ def make_response(func):
         try:
             code, msg, data = func(*args, **kwargs)
             resp = {'code': code, 'msg': msg, 'data': data}
+        except HTTPError as e:
+            resp = {'code': ResponseCode.Faild.value, 'msg': 'http服务暂不可用', 'data': {}}
         except Exception as e:
             resp = {'code': ResponseCode.Faild.value, 'msg': '异常错误', 'data': {}}
         finally:
+            logger.info(f'返回值: {resp}')
             return resp
     return decorate
+
+
+@retry(stop_max_attempt_number=3)
+def http(method, url, *args, **kwargs):
+    """http请求"""
+    opt = getattr(requests, method)
+    resp = opt(url, *args, **kwargs)
+    return resp
+
+
+if __name__ == '__main__':
+    logger.info('aaaa')
